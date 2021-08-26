e74c3dcc29b29568e852ada6e87a35f2a2dd5c8d
chilin0525 <sky707070.cv07@nctu.edu.tw>
2021-08-26 17:13:42 +0800

    add install.sh

---
+++ model_install.sh
@@ -0,0 +1,8 @@
+# Resnet50 
+wget https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet50-v1-7.onnx -P ./pretrained_model/ONNX
+
+# Googlenet
+wget https://github.com/onnx/models/raw/master/vision/classification/inception_and_googlenet/googlenet/model/googlenet-9.onnx -P ./pretrained_model/ONNX
+
+# Resnet101
+wget https://github.com/onnx/models/raw/master/vision/classification/resnet/model/resnet101-v1-7.onnx -P ./pretrained_model/ONNX
